0dba0090b21f5e777a2d96220fab43a056c27099
Eder <dev.eder.std@gmail.com>
2026-06-01 12:44:11 -0300

    primeira implementacao do serializer de user

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+class UserSerializer(serializers.ModelSerializer):
+    
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = User
+        fields = [
+            'id', 
+            'username', 
+            'first_name', 
+            'last_name', 
+            'bio', 
+            'profile_picture', 
+            'followers_count', 
+            'following_count',
+            'date_joined'
+        ]
+        def get_followers_count(self, obj):
+            return obj.followers.count()
+
+        def get_following_count(self, obj):
+            return obj.following.count()
