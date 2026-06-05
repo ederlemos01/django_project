a2b6a2f0b48c9c585c66b85e1110040d63d08b7c
Eder <dev.eder.std@gmail.com>
2026-06-05 14:27:53 -0300

    blindando todas as rotas com IsAuthenticated

--- setup/settings.py
+++ setup/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
